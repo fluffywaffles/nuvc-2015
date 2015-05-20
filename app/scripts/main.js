30d0eeaed93d14c32b8f74c2daa870205ea3c3b5
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-19 23:44:45 -0500

    pulling new branch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 /* jshint devel:true */
+
 $('#video-overlay video').hover(function() {
   'use strict';
   $(this).attr('controls', 'controls');
@@ -6,6 +7,14 @@ $('#video-overlay video').hover(function() {
   'use strict';
   $(this).removeAttr('controls');
 });
+
+if (!$.cookie('nuvc-skip-video')) {
+  $('#video-overlay').fadeIn()
+                     .find('video')[0]
+                     .play();
+
+  $.cookie('nuvc-skip-video', 'true', { expires: 300 });
+}
 
 $('#video-overlay video').on('ended', function() {
   'use strict';
@@ -34,7 +43,56 @@ $('#contact-form input[type="button"]').click(function() {
     }, 1500);
 });
 
-$('.question h3').click(function() {
+$('.question h4').click(function() {
   'use strict';
   $(this).next().toggleClass('active');
+});
+
+var w = new Waypoint({
+  element: $('#top')[0],
+  handler: function(direction) {
+    console.log(direction);
+    if (direction == 'down') {
+      console.log('sticky nav');
+      $('nav').addClass('top-fixed');
+    }
+    else {
+      $('nav').animate({opacity: 0 }, 'fast', function() {
+        $('nav').removeClass('top-fixed').removeAttr('style');
+      })
+    }
+  }
+});
+
+[].forEach.call(document.querySelectorAll('nav li'), function(el) {
+  new Waypoint({
+    element: document.getElementById($(el).find('a')[0].href.split('#')[1]),
+    handler: function (direction) {
+      direction === 'down'
+        ? ($('nav li').removeClass('active'), $(el).addClass('active'))
+        : ($('nav li').removeClass('active'), $(el).prev().addClass('active'))
+    }
+  });
+});
+
+new Waypoint({
+  element: $('h1')[0],
+  handler: function (direction) {
+    direction == 'down'
+      ? $('nav').css({top: 0, right: 0 }).addClass('extend')
+      : $('nav').removeAttr('style').removeClass('extend');
+  }
+});
+
+$('nav .hamburger').click(function(e) {
+  $('nav').toggleClass('open');
+  e.stopPropagation();
+  $('nav li').click(function() {
+    $('nav').removeClass('open');
+    $('nav li').off('click');
+  });
+  $('body').click(function() {
+    $('nav').removeClass('open');
+    $('body').off('click');
+  });
 });
